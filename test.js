144b32cf83a05d0b2fa1162d9cc9bcfb18b8e5ae
au2 <afsin@amida-tech.com>
2014-07-28 15:09:05 -0400

    Add supported_sections.  Only CCDA sections for now until BJ merges to DRE.

--- test.js
+++ test.js
@@ -1,21 +1,25 @@
 var expect = require('chai').expect;
 var assert = require('chai').assert;
 
-var meta = require("./index.js").CCDA;
+var bbm = require("./index.js")
+var ccda = bbm.CCDA;
 
 describe('blue-button-meta.js test', function(){
 
-
     it('9 templates', function(){
-      expect(Object.keys(meta.templates)).to.have.length(9);
+      expect(Object.keys(ccda.templates)).to.have.length(9);
     });
 
     it('70 sections', function(){
-      expect(Object.keys(meta.sections)).to.have.length(70);
+      expect(Object.keys(ccda.sections)).to.have.length(70);
     });
 
     it('76 clinical statements', function(){
-      expect(Object.keys(meta.statements)).to.have.length(76);
+      expect(Object.keys(ccda.statements)).to.have.length(76);
+    });
+
+    it('10 supported sections', function(){
+      expect(Object.keys(bbm.supported_sections)).to.have.length(10);
     });
 
 });
